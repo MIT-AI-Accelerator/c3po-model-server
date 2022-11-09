bc19c6271281ae7972193932c9da340a959066ce
al25551 <112004100+al25551@users.noreply.github.com>
2022-11-09 11:48:09 -0500

    Add files via upload

---
+++ models/Weak-Learning-Summarization/rouge_utils/utils.py
@@ -0,0 +1,128 @@
+import os
+import re
+import shutil
+import time
+
+import pyrouge
+
+#from eval_logging import init_logger
+
+REMAP = {"-lrb-": "(", "-rrb-": ")", "-lcb-": "{", "-rcb-": "}",
+         "-lsb-": "[", "-rsb-": "]", "``": '"', "''": '"'}
+
+
+def clean(x):
+    return re.sub(
+        r"-lrb-|-rrb-|-lcb-|-rcb-|-lsb-|-rsb-|``|''",
+        lambda m: REMAP.get(m.group()), x)
+
+
+def process(params):
+    temp_dir, data = params
+    candidates, references, pool_id = data
+    cnt = len(candidates)
+    current_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+    tmp_dir = os.path.join(temp_dir, "rouge-tmp-{}-{}".format(current_time, pool_id))
+    if not os.path.isdir(tmp_dir):
+        os.mkdir(tmp_dir)
+        os.mkdir(tmp_dir + "/candidate")
+        os.mkdir(tmp_dir + "/reference")
+    try:
+
+        for i in range(cnt):
+            if len(references[i]) < 1:
+                continue
+            with open(tmp_dir + "/candidate/cand.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(candidates[i])
+            with open(tmp_dir + "/reference/ref.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(references[i])
+#        r = pyrouge.Rouge155(temp_dir=temp_dir)
+        r = pyrouge.Rouge155()
+        r.model_dir = tmp_dir + "/reference/"
+        r.system_dir = tmp_dir + "/candidate/"
+        r.model_filename_pattern = 'ref.#ID#.txt'
+        r.system_filename_pattern = r'cand.(\d+).txt'
+        rouge_results = r.convert_and_evaluate()
+        print(rouge_results)
+        results_dict = r.output_to_dict(rouge_results)
+    finally:
+        pass
+        if os.path.isdir(tmp_dir):
+            shutil.rmtree(tmp_dir)
+    return results_dict
+
+
+def test_rouge(temp_dir, cand, ref):
+    candidates = [line.strip() for line in open(cand, encoding='utf-8')]
+    references = [line.strip() for line in open(ref, encoding='utf-8')]
+    print(len(candidates))
+    print(len(references))
+    assert len(candidates) == len(references)
+
+    cnt = len(candidates)
+    current_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
+    tmp_dir = os.path.join(temp_dir, "rouge-tmp-{}".format(current_time))
+    if not os.path.isdir(tmp_dir):
+        os.mkdir(tmp_dir)
+        os.mkdir(tmp_dir + "/candidate")
+        os.mkdir(tmp_dir + "/reference")
+    try:
+
+        for i in range(cnt):
+            if len(references[i]) < 1:
+                continue
+            with open(tmp_dir + "/candidate/cand.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(candidates[i])
+            with open(tmp_dir + "/reference/ref.{}.txt".format(i), "w",
+                      encoding="utf-8") as f:
+                f.write(references[i])
+#        r = pyrouge.Rouge155(temp_dir=temp_dir)
+        r = pyrouge.Rouge155()
+        r.model_dir = tmp_dir + "/reference/"
+        r.system_dir = tmp_dir + "/candidate/"
+        r.model_filename_pattern = 'ref.#ID#.txt'
+        r.system_filename_pattern = r'cand.(\d+).txt'
+        rouge_results = r.convert_and_evaluate()
+        print(rouge_results)
+        results_dict = r.output_to_dict(rouge_results)
+    finally:
+        pass
+        if os.path.isdir(tmp_dir):
+            shutil.rmtree(tmp_dir)
+    return results_dict
+
+
+def rouge_results_to_str(results_dict):
+    return ">> ROUGE-F(1/2/3/l): {:.2f}/{:.2f}/{:.2f}\nROUGE-R(1/2/3/l): {:.2f}/{:.2f}/{:.2f}\n".format(
+        results_dict["rouge_1_f_score"] * 100,
+        results_dict["rouge_2_f_score"] * 100,
+        results_dict["rouge_l_f_score"] * 100,
+        results_dict["rouge_1_recall"] * 100,
+        results_dict["rouge_2_recall"] * 100,
+        results_dict["rouge_l_recall"] * 100
+    )
+
+if __name__ == '__main__':
+    temp_dir = 'my_temp_dir'
+    #todo: write properly to can_path, gold_path (as in bertsum)
+    ###
+        #pred is a list of sentences; each sentence is a string
+#    can_path = '%s_step%d.candidate'%(self.args.result_path,step)
+    pred = ['blah blah blah', 'bleh bleh bleh']
+    gold = ['blah blah blah', 'bleh bleh bleh']
+    can_path = 'candidate'
+    gold_path = 'gold'
+    with open(can_path, 'w') as save_pred:
+                        for i in range(len(pred)):
+                            save_pred.write(pred[i].strip()+'\n')
+    with open(gold_path, 'w') as save_gold:
+                        for i in range(len(gold)):
+                            save_gold.write(gold[i].strip()+'\n')
+    ###
+    rouges = test_rouge(temp_dir, can_path, gold_path)
+#    logger = init_logger()
+#    logger.info('Rouges \n%s' % (rouge_results_to_str(rouges)))
+
