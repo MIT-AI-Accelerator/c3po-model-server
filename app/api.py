21bcbc9af7d49b6cdeb93780db3853549f4e754d
Eric Robinson <erob@mit.edu>
2023-03-07 10:32:10 -0500

    change literals for constants

--- app/api.py
+++ app/api.py
@@ -34,6 +34,11 @@ class PromptRequest(BaseModel):
 class PromptResponse(BaseModel):
     answer: str
 
+class ErrorResponse(BaseModel):
+    message: str
+
+NO_FILE_ERROR_RESPONSE = ErrorResponse(message="No upload file sent")
+OUTPUT_DIR = "./app/models/LSTM_basic_classifier/training_checkpoints/"
 
 @app.post("/predict/", response_model=PromptResponse)
 def predict(request: PromptRequest, model: Model = Depends(get_model)):
@@ -46,15 +51,14 @@ def predict(request: PromptRequest, model: Model = Depends(get_model)):
 # upload file docs here: https://fastapi.tiangolo.com/tutorial/request-files/
 @app.post("/lstm-basic-classifier/upload-checkpoint-metadata/")
 async def upload_checkpoint_metadata(new_file: Union[UploadFile, None] = None):
-    output_dir = "./app/models/LSTM_basic_classifier/training_checkpoints/"
     output_filename = "checkpoint"
-    output_file = output_dir + output_filename
+    output_file = OUTPUT_DIR + output_filename
 
     if not new_file:
-        return {"message": "No upload file sent"}
+        return NO_FILE_ERROR_RESPONSE
     else:
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+        if not os.path.isdir(OUTPUT_DIR):
+            os.makedirs(OUTPUT_DIR)
 
         async with open(output_file, 'wb') as out_file:
             while content := await new_file.read(1024):  # async read chunk
@@ -65,15 +69,14 @@ async def upload_checkpoint_metadata(new_file: Union[UploadFile, None] = None):
 # upload file docs here: https://fastapi.tiangolo.com/tutorial/request-files/
 @app.post("/lstm-basic-classifier/upload-checkpoint-index/")
 async def upload_checkpoint_index(new_file: Union[UploadFile, None] = None):
-    output_dir = "./app/models/LSTM_basic_classifier/training_checkpoints/"
     output_filename = "my_ckpt.index"
-    output_file = output_dir + output_filename
+    output_file = OUTPUT_DIR + output_filename
 
     if not new_file:
-        return {"message": "No upload file sent"}
+        return NO_FILE_ERROR_RESPONSE
     else:
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+        if not os.path.isdir(OUTPUT_DIR):
+            os.makedirs(OUTPUT_DIR)
 
         async with open(output_file, 'wb') as out_file:
             while content := await new_file.read(1024):  # async read chunk
@@ -84,15 +87,14 @@ async def upload_checkpoint_index(new_file: Union[UploadFile, None] = None):
 # upload file docs here: https://fastapi.tiangolo.com/tutorial/request-files/
 @app.post("/lstm-basic-classifier/upload-checkpoint-data/")
 async def upload_checkpoint_data(new_file: Union[UploadFile, None] = None):
-    output_dir = "./app/models/LSTM_basic_classifier/training_checkpoints/"
     output_filename = "my_ckpt.data-00000-of-00001"
-    output_file = output_dir + output_filename
+    output_file = OUTPUT_DIR + output_filename
 
     if not new_file:
-        return {"message": "No upload file sent"}
+        return NO_FILE_ERROR_RESPONSE
     else:
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+        if not os.path.isdir(OUTPUT_DIR):
+            os.makedirs(OUTPUT_DIR)
 
         async with open(output_file, 'wb') as out_file:
             while content := await new_file.read(1024):  # async read chunk
@@ -103,15 +105,14 @@ async def upload_checkpoint_data(new_file: Union[UploadFile, None] = None):
 # upload file docs here: https://fastapi.tiangolo.com/tutorial/request-files/
 @app.post("/lstm-basic-classifier/upload-train-data/")
 async def upload_train_data(new_file: Union[UploadFile, None] = None):
-    output_dir = "./app/models/LSTM_basic_classifier/training_checkpoints/"
     output_filename = "train_data.csv"
-    output_file = output_dir + output_filename
+    output_file = OUTPUT_DIR + output_filename
 
     if not new_file:
-        return {"message": "No upload file sent"}
+        return NO_FILE_ERROR_RESPONSE
     else:
-        if not os.path.isdir(output_dir):
-            os.makedirs(output_dir)
+        if not os.path.isdir(OUTPUT_DIR):
+            os.makedirs(OUTPUT_DIR)
 
         async with open(output_file, 'wb') as out_file:
             while content := await new_file.read(1024):  # async read chunk
