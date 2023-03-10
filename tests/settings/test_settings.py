6ad1f7f8252a33e930ed90e140a4cb298d2ad482
Eric Robinson (TRON | Admin) <eric.robinson@afwerx.af.mil>
2023-03-10 22:06:59 +0000

    Chore/env fixes

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,4 +1,7 @@
+import os
 from app.settings.settings import EnvironmentSettings, Settings, settings, get_env_file
+
+BASEDIR = os.path.join(os.path.abspath(os.path.dirname("./app/settings/settings.py")), "env_var")
 
 def test_settings_exists():
     assert isinstance(settings, Settings)
@@ -8,16 +11,16 @@ def test_environment_local_default():
     assert environment_settings.environment == 'local'
 
 def test_env_file_name_local():
-    environment_settings = EnvironmentSettings()
+    environment_settings = EnvironmentSettings(environment='local')
     env_file = get_env_file(environment_settings)
-    assert env_file == 'local.env'
+    assert env_file == os.path.join(BASEDIR, "local.env")
 
 def test_env_file_name_staging():
     environment_settings = EnvironmentSettings(environment='staging')
     env_file = get_env_file(environment_settings)
-    assert env_file == 'staging.env'
+    assert env_file == os.path.join(BASEDIR, "staging.env")
 
 def test_env_file_name_production():
     environment_settings = EnvironmentSettings(environment='production')
     env_file = get_env_file(environment_settings)
-    assert env_file == 'production.env'
+    assert env_file == os.path.join(BASEDIR, "production.env")
