50ededaebb3fc2810371950cf52a5096ee9b5bc2
Eric Robinson <erob@mit.edu>
2023-03-26 18:12:12 -0400

    massive reorg

--- app/aimodels/router.py
+++ app/aimodels/router.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter
 from .lstm_stress_classifier.prefix_router import router as lstm_stress_classifier_router
-from .bertopic.prefix_router import router as bertopic_router
-
 
 router = APIRouter(
     prefix="/aimodels"
 )
 
 router.include_router(lstm_stress_classifier_router)
-router.include_router(bertopic_router)
