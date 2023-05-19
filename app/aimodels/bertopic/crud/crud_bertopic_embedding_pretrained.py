32ae8cb99e9ce1c0ae9f0f9a0abac413abbf71ec
Eric Robinson <erob@mit.edu>
2023-05-19 15:36:12 -0400

    fix code smells

--- app/aimodels/bertopic/crud/crud_bertopic_embedding_pretrained.py
+++ app/aimodels/bertopic/crud/crud_bertopic_embedding_pretrained.py
@@ -1,15 +1,20 @@
 
+from typing import Union
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
 from ..models.bertopic_embedding_pretrained import BertopicEmbeddingPretrainedModel
 from ..schemas.bertopic_embedding_pretrained import BertopicEmbeddingPretrainedCreate, BertopicEmbeddingPretrainedUpdate
 
 # CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType])
-class CRUDBertopicEmbeddingPretrained(CRUDBase[BertopicEmbeddingPretrainedModel, BertopicEmbeddingPretrainedCreate, BertopicEmbeddingPretrainedUpdate]  ):
-    def get_by_sha256(self, db: Session, *, sha256: str) -> BertopicEmbeddingPretrainedModel:
+
+
+class CRUDBertopicEmbeddingPretrained(CRUDBase[BertopicEmbeddingPretrainedModel, BertopicEmbeddingPretrainedCreate, BertopicEmbeddingPretrainedUpdate]):
+    def get_by_sha256(self, db: Session, *, sha256: str) -> Union[BertopicEmbeddingPretrainedModel, None]:
         if not sha256:
             return None
 
         return db.query(self.model).filter(self.model.sha256 == sha256).first()
 
-bertopic_embedding_pretrained = CRUDBertopicEmbeddingPretrained(BertopicEmbeddingPretrainedModel)
+
+bertopic_embedding_pretrained = CRUDBertopicEmbeddingPretrained(
+    BertopicEmbeddingPretrainedModel)
