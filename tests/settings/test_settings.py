0e2dea4e0efebcafc8c16b23f6f49a7f6ca1754c
Eric Robinson <erob@mit.edu>
2023-05-19 14:04:38 -0400

    fix pipeline

--- tests/settings/test_settings.py
+++ tests/settings/test_settings.py
@@ -1,4 +1,5 @@
 import os
+from unittest import mock
 from app.core.config import EnvironmentSettings, Settings, settings, get_env_file
 
 BASEDIR = os.path.join(os.path.abspath(os.path.dirname("./app/core/config.py")), "env_var")
@@ -6,21 +7,31 @@ BASEDIR = os.path.join(os.path.abspath(os.path.dirname("./app/core/config.py")),
 def test_settings_exists():
     assert isinstance(settings, Settings)
 
-def test_environment_local_default():
+@mock.patch.dict(os.environ, clear=True) # clear=True is needed to clear the environment variables
+def test_environment_test_default():
     environment_settings = EnvironmentSettings()
-    assert environment_settings.environment == 'local'
+    assert environment_settings.environment == 'test'
 
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "test"})
+def test_env_file_name_test():
+    environment_settings = EnvironmentSettings()
+    env_file = get_env_file(environment_settings)
+    assert env_file == os.path.join(BASEDIR, "test.env")
+
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "local"})
 def test_env_file_name_local():
-    environment_settings = EnvironmentSettings(environment='local')
+    environment_settings = EnvironmentSettings()
     env_file = get_env_file(environment_settings)
     assert env_file == (os.path.join(BASEDIR, "local.env"), os.path.join(BASEDIR, "secrets.env"))
 
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "staging"})
 def test_env_file_name_staging():
-    environment_settings = EnvironmentSettings(environment='staging')
+    environment_settings = EnvironmentSettings()
     env_file = get_env_file(environment_settings)
     assert env_file == os.path.join(BASEDIR, "staging.env")
 
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "production"})
 def test_env_file_name_production():
-    environment_settings = EnvironmentSettings(environment='production')
+    environment_settings = EnvironmentSettings()
     env_file = get_env_file(environment_settings)
     assert env_file == os.path.join(BASEDIR, "production.env")
--- tests/test_files/crud/crud_test_model.py
+++ tests/test_files/crud/crud_test_model.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, UUID, Boolean, String
 from app.db.base_class import Base
 import uuid
 
-class TestModel(Base):
+class EmptyTestModel(Base):
     id = Column(UUID, primary_key=True, unique=True, default=uuid.uuid4)
     bool_field = Column(Boolean(), default=False)
     title = Column(String(), default="title")
