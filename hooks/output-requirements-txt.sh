f599abd393892e2c615bc2b6d6714f6bc69f8179
Eric Robinson <erob@mit.edu>
2023-05-22 22:54:41 -0400

    transformers CVE patch

--- hooks/output-requirements-txt.sh
+++ hooks/output-requirements-txt.sh
@@ -2,3 +2,4 @@
 
 pipenv requirements > requirements.txt
 echo "tensorflow==2.11.1" >> requirements.txt
+sed -i 's/^.*transformers.*$/transformers @ git+https://github.com/huggingface/transformers.git@80ca92470938bbcc348e2d9cf4734c7c25cb1c43/' requirements.txt
