fb9391cbf91d763898ec7121c68770a18bc945f6
Eric Robinson <erob@mit.edu>
2023-05-24 10:31:55 -0400

    WIP

--- app/initial_data.py
+++ app/initial_data.py
@@ -10,13 +10,15 @@ from app.aimodels.bertopic.models.bertopic_embedding_pretrained import BertopicE
 from app.aimodels.bertopic.models.document import DocumentModel
 from app.aimodels.bertopic.schemas.bertopic_embedding_pretrained import BertopicEmbeddingPretrainedCreate, BertopicEmbeddingPretrainedUpdate
 from app.aimodels.bertopic.schemas.document import DocumentCreate
+from app.aimodels.gpt4all.models.gpt4all_pretrained import Gpt4AllPretrainedModel
 
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
 from app.core.minio import build_client, upload_file_to_minio
 
 from app.core.config import settings, environment_settings
-from app.aimodels.bertopic import crud
+from app.aimodels.bertopic import crud as bertopic_crud
+from app.aimodels.gpt4all import crud as gpt4all_crud
 from sentence_transformers import SentenceTransformer
 
 from sqlalchemy.orm import Session
@@ -57,7 +59,7 @@ def init_sentence_embedding_object(s3: Minio, db: Session) -> None:
     hex_dig = hash_object.hexdigest()
 
     # check to make sure sha256 doesn't already exist
-    obj_by_sha: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.get_by_sha256(
+    obj_by_sha: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.get_by_sha256(
         db, sha256=hex_dig)
 
     if not obj_by_sha:
@@ -73,7 +75,7 @@ def init_sentence_embedding_object(s3: Minio, db: Session) -> None:
         bertopic_embedding_pretrained_obj = BertopicEmbeddingPretrainedCreate(
             sha256=hex_dig, model_name=model_name)
 
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.create(
+        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.create(
             db, obj_in=bertopic_embedding_pretrained_obj)
 
         # utilize id from above to upload file to minio
@@ -82,7 +84,7 @@ def init_sentence_embedding_object(s3: Minio, db: Session) -> None:
 
         # update the object to reflect uploaded status
         updated_object = BertopicEmbeddingPretrainedUpdate(uploaded=True)
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.update(
+        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.update(
             db, db_obj=new_bertopic_embedding_pretrained_obj, obj_in=updated_object)
 
         return new_bertopic_embedding_pretrained_obj
@@ -106,27 +108,17 @@ def init_gpt4all_pretrained_model(s3: Minio, db: Session) -> None:
 
     # open the file in binary mode and write the contents of the response to it in chunks
     # This is a large file, so be prepared to wait.
+    hash_object = hashlib.sha256()
     with open(local_path, 'wb') as f:
         for chunk in tqdm(response.iter_content(chunk_size=8192)):
             if chunk:
                 f.write(chunk)
-
-
-
-
-
-    # Create the SentenceTransformer object
-    embedding_pretrained_model_obj = SentenceTransformer("all-MiniLM-L6-v2")
-
-    # Serialize the object
-    serialized_obj = pickle.dumps(embedding_pretrained_model_obj)
-
-    # Calculate the SHA256 hash of the serialized object
-    hash_object = hashlib.sha256(serialized_obj)
+                hash_object.update(chunk)
+            
     hex_dig = hash_object.hexdigest()
 
     # check to make sure sha256 doesn't already exist
-    obj_by_sha: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.get_by_sha256(
+    obj_by_sha: Gpt4AllPretrainedModel = gpt4all_crud.gpt4all_pretrained.get_by_sha256(
         db, sha256=hex_dig)
 
     if not obj_by_sha:
@@ -142,7 +134,7 @@ def init_gpt4all_pretrained_model(s3: Minio, db: Session) -> None:
         bertopic_embedding_pretrained_obj = BertopicEmbeddingPretrainedCreate(
             sha256=hex_dig)
 
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.create(
+        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.create(
             db, obj_in=bertopic_embedding_pretrained_obj)
 
         # utilize id from above to upload file to minio
@@ -151,7 +143,7 @@ def init_gpt4all_pretrained_model(s3: Minio, db: Session) -> None:
 
         # update the object to reflect uploaded status
         updated_object = BertopicEmbeddingPretrainedUpdate(uploaded=True)
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = crud.bertopic_embedding_pretrained.update(
+        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.update(
             db, db_obj=new_bertopic_embedding_pretrained_obj, obj_in=updated_object)
 
         return new_bertopic_embedding_pretrained_obj
@@ -214,7 +206,7 @@ def init_documents_from_chats(db: Session) -> str:
     cdata['messages'] = cdata['messages'].astype(str)
     msgs = cdata['messages'].values.tolist()[6:30]
     documents = [DocumentCreate(text=msg.strip()) for msg in msgs]
-    documents_db = crud.document.create_all_using_id(db, obj_in_list=documents)
+    documents_db = bertopic_crud.document.create_all_using_id(db, obj_in_list=documents)
 
     # format for swagger input
     swagger_string = "["
