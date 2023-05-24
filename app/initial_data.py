944d3507c776160c6b14cf4e8e99eaf7690b08f5
Eric Robinson <erob@mit.edu>
2023-05-24 13:39:46 -0400

    WIP

--- app/initial_data.py
+++ app/initial_data.py
@@ -1,8 +1,12 @@
+import os
 import sys
 import hashlib
 import io
 import pickle
 import logging
+import requests
+from pathlib import Path
+from tqdm import tqdm
 from fastapi import UploadFile
 from fastapi.encoders import jsonable_encoder
 from minio.error import InvalidResponseError
@@ -11,6 +15,7 @@ from app.aimodels.bertopic.models.document import DocumentModel
 from app.aimodels.bertopic.schemas.bertopic_embedding_pretrained import BertopicEmbeddingPretrainedCreate, BertopicEmbeddingPretrainedUpdate
 from app.aimodels.bertopic.schemas.document import DocumentCreate
 from app.aimodels.gpt4all.models.gpt4all_pretrained import Gpt4AllPretrainedModel
+from app.aimodels.gpt4all.schemas.gpt4all_pretrained import Gpt4AllPretrainedCreate, Gpt4AllPretrainedUpdate
 
 from app.db.init_db import init_db
 from app.db.session import SessionLocal
@@ -25,6 +30,7 @@ from sqlalchemy.orm import Session
 from minio import Minio
 import pandas as pd
 from sample_data import CHAT_DATASET_1_PATH
+from app.core.model_cache import MODEL_CACHE_BASEDIR
 
 from app.aimodels.bertopic.ai_services.weak_learning import WeakLearner
 from sample_data import CHAT_DATASET_4_PATH
@@ -91,13 +97,10 @@ def init_sentence_embedding_object(s3: Minio, db: Session) -> None:
 
     return obj_by_sha
 
+
 def init_gpt4all_pretrained_model(s3: Minio, db: Session) -> None:
-    import requests
-    from pathlib import Path
-    from tqdm import tqdm
-
-    local_path = './models/ggml-gpt4all-l13b-snoozy.bin'  # replace with your desired local file path
-
+    local_path = os.path.join(
+        MODEL_CACHE_BASEDIR, "ggml-gpt4all-l13b-snoozy.bin")
     Path(local_path).parent.mkdir(parents=True, exist_ok=True)
 
     # Example model. Check https://github.com/nomic-ai/gpt4all for the latest models.
@@ -123,35 +126,31 @@ def init_gpt4all_pretrained_model(s3: Minio, db: Session) -> None:
 
     if not obj_by_sha:
         # Create an in-memory file object
-        file_obj = io.BytesIO()
+        # file_obj = io.BytesIO()
 
-        # Dump the object to the file object
-        pickle.dump(embedding_pretrained_model_obj, file_obj)
+        # # Dump the object to the file object
+        # pickle.dump(embedding_pretrained_model_obj, file_obj)
 
-        # Move the file cursor to the beginning of the file
-        file_obj.seek(0)
+        # # Move the file cursor to the beginning of the file
+        # file_obj.seek(0)
 
-        bertopic_embedding_pretrained_obj = BertopicEmbeddingPretrainedCreate(
+        gpt4all_pretrained_obj = Gpt4AllPretrainedCreate(
             sha256=hex_dig)
 
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.create(
-            db, obj_in=bertopic_embedding_pretrained_obj)
+        new_gpt4all_pretrained_obj: Gpt4AllPretrainedModel = gpt4all_crud.gpt4all_pretrained.create(
+            db, obj_in=gpt4all_pretrained_obj)
 
         # utilize id from above to upload file to minio
-        upload_file_to_minio(UploadFile(file_obj),
-                             new_bertopic_embedding_pretrained_obj.id, s3)
+        with open(local_path, 'rb') as file_obj:
+            upload_file_to_minio(UploadFile(file_obj),
+                                 new_gpt4all_pretrained_obj.id, s3)
 
         # update the object to reflect uploaded status
-        updated_object = BertopicEmbeddingPretrainedUpdate(uploaded=True)
-        new_bertopic_embedding_pretrained_obj: BertopicEmbeddingPretrainedModel = bertopic_crud.bertopic_embedding_pretrained.update(
-            db, db_obj=new_bertopic_embedding_pretrained_obj, obj_in=updated_object)
+        updated_object = Gpt4AllPretrainedUpdate(uploaded=True)
+        new_gpt4all_pretrained_obj: Gpt4AllPretrainedModel = gpt4all_crud.gpt4all_pretrained.update(
+            db, db_obj=new_gpt4all_pretrained_obj, obj_in=updated_object)
 
-        return new_bertopic_embedding_pretrained_obj
-
-
-
-
-
+        return new_gpt4all_pretrained_obj
 
 
 def init_weak_learning_object(s3: Minio, db: Session) -> None:
@@ -206,7 +205,8 @@ def init_documents_from_chats(db: Session) -> str:
     cdata['messages'] = cdata['messages'].astype(str)
     msgs = cdata['messages'].values.tolist()[6:30]
     documents = [DocumentCreate(text=msg.strip()) for msg in msgs]
-    documents_db = bertopic_crud.document.create_all_using_id(db, obj_in_list=documents)
+    documents_db = bertopic_crud.document.create_all_using_id(
+        db, obj_in_list=documents)
 
     # format for swagger input
     swagger_string = "["
@@ -251,8 +251,13 @@ def main() -> None:
         logger.info("Uploading SentenceTransformer object to MinIO")
         embedding_pretrained_obj = init_sentence_embedding_object(s3, db)
         logger.info("SentenceTransformer object uploaded to MinIO.")
+        logger.info("Uploading Gpt4All object to MinIO")
+        gpt4all_pretrained_obj = init_gpt4all_pretrained_model(s3, db)
+        logger.info("Gpt4All object uploaded to MinIO.")
         logger.info(
             f"Embedding Pretrained Object ID: {embedding_pretrained_obj.id}, SHA256: {embedding_pretrained_obj.sha256}")
+        logger.info(
+            f"Gpt4All Object ID: {gpt4all_pretrained_obj.id}, SHA256: {gpt4all_pretrained_obj.sha256}")
 
     if (environment == 'local'):
         logger.info("Uploading WeakLearner object to MinIO")
