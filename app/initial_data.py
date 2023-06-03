e5cc6ab868b92378430a1d2e4843306d1d4382e7
Eric Robinson <erob@mit.edu>
2023-06-03 17:10:26 -0400

    remove sys.err test calls

--- app/initial_data.py
+++ app/initial_data.py
@@ -23,7 +23,6 @@ from sqlalchemy.orm import Session
 from minio import Minio
 import pandas as pd
 from sample_data import CHAT_DATASET_1_PATH
-import sys
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -39,7 +38,7 @@ def init_minio_bucket(s3: Minio) -> None:
         if not s3.bucket_exists(bucket_name):
             s3.make_bucket(bucket_name)
     except InvalidResponseError as err:
-        print(err)
+        logger.error(err)
 
 
 def init_sentence_embedding_object(s3: Minio, db: Session) -> None:
@@ -106,9 +105,8 @@ def init_documents_from_chats(db: Session) -> str:
 
 
 def main() -> None:
-    print('Main called', file=sys.stderr)
     args = sys.argv[1:]
-    
+
     migration_toggle = False
     if len(args) == 1 and args[0] == '--toggle-migration':
         migration_toggle = True
@@ -116,7 +114,6 @@ def main() -> None:
     # environment can be one of 'local', 'test', 'staging', 'production'
     environment = environment_settings.environment
 
-    print('Environment vars loaded', file=sys.stderr)
     logger.info(f"Using initialization environment: {environment}")
     logger.info(f"Using migration toggle: {migration_toggle}")
 
@@ -124,14 +121,10 @@ def main() -> None:
     # prod only if migration toggle is on
     if (environment in ['local', 'test', 'staging'] or (environment == 'production' and migration_toggle is True)):
         logger.info("Creating database schema and tables")
-        print('Creating database schema and tables', file=sys.stderr)
         db = SessionLocal()
-        print('session created', file=sys.stderr)
         init()
-        print('db init done', file=sys.stderr)
         logger.info("Initial database schema and tables created.")
     else:
-        print('Skipping database initialization', file=sys.stderr)
         logger.info("Skipping database initialization")
 
     if (environment == 'local'):
@@ -149,9 +142,7 @@ def main() -> None:
 
     if (environment != 'production'):
         logger.info("Creating documents from chats")
-        print('creating documents from chats', file=sys.stderr)
         swagger_string = init_documents_from_chats(db)
-        print('Documents created', file=sys.stderr)
         logger.info("Documents created.")
         logger.info(f"Documents: {swagger_string}")
 
