8e5ff27f7512d7cd57db6d67efc4509393208252
Emilie Cowen <em23761@mit.edu>
2023-07-04 16:32:27 -0400

    recommend numpy version 1.24.4 for weak learning; added tests for train (failing unfortunately, minio issue)

--- tests/aimodels/bertopic/routers/test_train.py
+++ tests/aimodels/bertopic/routers/test_train.py
@@ -1,35 +1,12 @@
-import os
-from unittest.mock import MagicMock
-import uuid
 from fastapi.testclient import TestClient
-from app.aimodels.bertopic.models.bertopic_embedding_pretrained import BertopicEmbeddingPretrainedModel, EmbeddingModelTypeEnum
 from app.aimodels.bertopic.models.document import DocumentModel
-
-from app.main import app
-from app.aimodels.bertopic.routers.train import get_db, get_minio
-from tests.test_files.db.db_test_session import SessionLocal
 
 from fastapi.encoders import jsonable_encoder
 
 from sqlalchemy.orm import Session
+
+from sample_data import CHAT_DATASET_4_PATH
 from minio import Minio
-
-
-# ************Mocks*******************
-def mock_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-mock_s3 = MagicMock()
-def mock_get_minio():
-    return mock_s3
-
-app.dependency_overrides = {get_db: mock_db, get_minio: mock_get_minio}
-# *************************************
-
 
 # test train endpoint with invalid request
 def test_train_invalid_request(client: TestClient):
@@ -48,63 +25,120 @@ def test_train_invalid_request(client: TestClient):
 
 
 # test train endpoint with valid request
-def test_train_valid_request(client: TestClient, db: Session):
-    pass
+def test_train_valid_request(client: TestClient, db: Session, s3: Minio):
+# def test_train_valid_request(client: TestClient, db: Session):
+    
+    my_model = 'all-MiniLM-L6-v2'
+    response = client.get(
+        "/aimodels/bertopic/bertopic-embedding-pretrained",
+        headers={},
+        params=jsonable_encoder({'model_name': my_model})
+    )
+    assert response.status_code == 200
+    assert response.json()['model_name'] == my_model
+    sentence_transformer_id = response.json()['id']
 
-    # # create bertopic embedding pretrained object
-    # embedding_pretrained_obj = BertopicEmbeddingPretrainedModel(id=uuid.uuid4(), sha256=valid_sha256)
+    # get documents
+    n_docs = 11
+    documents_db = db.query(DocumentModel).limit(n_docs).all()
+    assert len(documents_db) == n_docs
 
-    # # mock crud.bertopic_embedding_pretrained.get(db, request.bertopic_embedding_pretrained_id) to return the object created above
-    # with mock.patch('app.aimodels.bertopic.crud.crud_bertopic_embedding_pretrained.bertopic_embedding_pretrained.get') as mock_get:
-    #     mock_get.return_value = embedding_pretrained_obj
+    body = {
+        "sentence_transformer_id": sentence_transformer_id,
+        "document_ids": [d.id for d in documents_db]
+    }
 
-    # sentence_transformer_db = db.query(BertopicEmbeddingPretrainedModel).filter(
-    #     BertopicEmbeddingPretrainedModel.model_type == EmbeddingModelTypeEnum.SENTENCE_TRANSFORMERS,
-    #     BertopicEmbeddingPretrainedModel.uploaded == True).first()
+    response = client.post(
+        "/aimodels/bertopic/train",
+        headers={},
+        json=jsonable_encoder(body),
+    )
 
-    # documents_db = db.query(DocumentModel).limit(10).all()
-
-    # body = {
-    #     "sentence_transformer_id": sentence_transformer_db.id,
-    #     "document_ids": [d.id for d in documents_db]
-    # }
-
-    # response = client.post(
-    #     "/aimodels/bertopic/train",
-    #     headers={},
-    #     json=jsonable_encoder(body),
-    # )
-
-    # assert response.status_code == 200
-    # assert response.json()['id'] is not None
+    assert response.status_code == 200
+    assert response.json()['id'] is not None
 
 
 # test train endpoint with valid request
-def test_train_valid_request_weak_learning(client: TestClient, db: Session):
-    pass
+def test_train_valid_request_seed_topics(client: TestClient, db: Session, s3: Minio):
+    
+    my_model = 'all-MiniLM-L6-v2'
+    response = client.get(
+        "/aimodels/bertopic/bertopic-embedding-pretrained",
+        headers={},
+        params=jsonable_encoder({'model_name': my_model})
+    )
+    assert response.status_code == 200
+    assert response.json()['model_name'] == my_model
+    sentence_transformer_id = response.json()['id']
 
-    # sentence_transformer_db = db.query(BertopicEmbeddingPretrainedModel).filter(
-    #     BertopicEmbeddingPretrainedModel.model_type == EmbeddingModelTypeEnum.SENTENCE_TRANSFORMERS,
-    #     BertopicEmbeddingPretrainedModel.uploaded == True).first()
+    # get documents
+    n_docs = 11
+    documents_db = db.query(DocumentModel).limit(n_docs).all()
+    assert len(documents_db) == n_docs
 
-    # weak_learner_db = db.query(BertopicEmbeddingPretrainedModel).filter(
-    #     BertopicEmbeddingPretrainedModel.model_type == EmbeddingModelTypeEnum.WEAK_LEARNERS,
-    #     BertopicEmbeddingPretrainedModel.uploaded == True).first()
+    seed_topics = [['urgent', 'priority'],
+        ['delay', 'slip']]
+    num_topics=len(seed_topics)
 
-    # documents_db = db.query(DocumentModel).limit(10).all()
+    body = {
+        "sentence_transformer_id": sentence_transformer_id,
+        "document_ids": [d.id for d in documents_db],
+        'num_topics': num_topics,
+        'seed_topics': seed_topics
+    }
 
-    # body = {
-    #     "sentence_transformer_id": sentence_transformer_db.id,
-    #     "weak_learner_id": weak_learner_db.id,
-    #     "document_ids": [d.id for d in documents_db]
-    # }
+    response = client.post(
+        "/aimodels/bertopic/train",
+        headers={},
+        json=jsonable_encoder(body),
+    )
 
-    # response = client.post(
-    #     "/aimodels/bertopic/train",
-    #     headers={},
-    #     json=jsonable_encoder(body),
-    # )
+    assert response.status_code == 200
+    assert response.json()['id'] is not None
 
-    # assert response.status_code == 200
-    # assert response.json()['id'] is not None
-    # assert response.json() == {}
+
+# test train endpoint with valid request
+def test_train_valid_request_weak_learning(client: TestClient, db: Session, s3: Minio):
+
+    # get valid sentence transformer object
+    my_model = 'all-MiniLM-L6-v2'
+    response = client.get(
+        "/aimodels/bertopic/bertopic-embedding-pretrained",
+        headers={},
+        params=jsonable_encoder({'model_name': my_model})
+    )
+    assert response.status_code == 200
+    assert response.json()['model_name'] == my_model
+    sentence_transformer_id = response.json()['id']
+
+    # get valid weak learner object
+    my_model = CHAT_DATASET_4_PATH.split('/')[-1]
+    response = client.get(
+        "/aimodels/bertopic/bertopic-embedding-pretrained",
+        headers={},
+        params=jsonable_encoder({'model_name': my_model})
+    )
+    assert response.status_code == 200
+    assert response.json()['model_name'] == my_model
+    weak_learner_id = response.json()['id']
+
+    # get documents
+    n_docs = 11
+    documents_db = db.query(DocumentModel).limit(n_docs).all()
+    assert len(documents_db) == n_docs
+
+    # train on documents
+    body = {
+        "sentence_transformer_id": sentence_transformer_id,
+        "weak_learner_id": weak_learner_id,
+        "document_ids": [d.id for d in documents_db]
+    }
+
+    response = client.post(
+        "/aimodels/bertopic/train",
+        headers={},
+        json=jsonable_encoder(body),
+    )
+
+    assert response.status_code == 200
+    assert response.json()['id'] is not None
