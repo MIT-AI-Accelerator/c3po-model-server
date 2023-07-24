e9b5d688900c4698df50eef7b70e934736038182
Emilie Cowen <em23761@mit.edu>
2023-07-23 20:08:59 -0400

    update mattermost router tests

--- tests/mattermost/test_mattermost_router.py
+++ tests/mattermost/test_mattermost_router.py
@@ -3,25 +3,90 @@ from app.main import app
 
 client = TestClient(app)
 
-# returns 200 and nonempty body
-def test_get_mattermost_user_info():
-    response = client.get(
-        "/mattermost/user-info/",
+# returns 422
+def test_upload_mattermost_user_info_invalid_format():
+    response = client.post(
+        "/mattermost/user/upload",
         headers={},
-        params={"user_name":"nitmre-bot"},
+        json={"notafield": "notauser"}
     )
 
-    assert response.status_code == 200
-    assert response.json() != {}
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'field required'
 
-# returns 200 and nonempty body
-def test_get_mattermost_channel_posts():
-    channel_ids = '{"0":"q3jroix947ymumrhn7mtz9uxmr","1":"z71qhruo9tyuujp7589sus6mew"}'
-    response = client.get(
-        "/mattermost/documents/",
+# returns 422
+def test_upload_mattermost_user_info_invalid_input():
+    response = client.post(
+        "/mattermost/user/upload",
         headers={},
-        params={"channels":channel_ids},
+        json={"user_name": "notauser"}
     )
 
-    assert response.status_code == 200
-    assert response.json() != {}
+    assert response.status_code == 422
+    # assert 'Mattermost' in response.json()['detail'] FIXME
+
+# returns 422
+def test_get_mattermost_user_info_invalid_format():
+    response = client.get(
+        "/mattermost/user/get",
+        headers={},
+        params={"notafield": "notauser"}
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'field required'
+
+# returns 422
+def test_get_mattermost_user_info_invalid_input():
+    response = client.get(
+        "/mattermost/user/get",
+        headers={},
+        params={"user_name": "notauser"}
+    )
+
+    assert response.status_code == 422
+    assert 'Mattermost' in response.json()['detail']
+
+# returns 422
+def test_upload_mattermost_documents_invalid_format():
+    response = client.post(
+        "/mattermost/documents/upload",
+        headers={},
+        json={"notafield": "notachannelid1"}
+    )
+
+    assert response.status_code == 422
+    # assert response.json()['detail'][0]['msg'] == 'field required' FIXME
+
+# returns 422
+def test_upload_mattermost_documents_invalid_input():
+    response = client.post(
+        "/mattermost/documents/upload",
+        headers={},
+        json={"channel_ids": ["notachannelid"]}
+    )
+
+    assert response.status_code == 422
+    # assert 'Mattermost' in response.json()['detail'] FIXME
+
+# returns 422
+def test_get_mattermost_documents_invalid_format():
+    response = client.get(
+        "/mattermost/documents/get",
+        headers={},
+        params={"notafield": "notachannelid"}
+    )
+
+    assert response.status_code == 422
+    assert response.json()['detail'][0]['msg'] == 'field required'
+
+# returns 422
+def test_get_mattermost_documents_invalid_input():
+    response = client.get(
+        "/mattermost/documents/get",
+        headers={},
+        params={"team_name": "notachannelname", "channel_name": "notachannelname"}
+    )
+
+    assert response.status_code == 422
+    assert 'Mattermost' in response.json()['detail']
