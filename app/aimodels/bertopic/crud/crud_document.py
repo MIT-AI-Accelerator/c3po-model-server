a280edf03ac5cd0ff8357fb214880736552c7e0f
erob123 <46288978+erob123@users.noreply.github.com>
2023-08-31 17:21:33 -0400

    Feat/mm retrieval (#60)
    - html change to incorporate the LLM summary in the UI
    - html change to incoporate MM links and content
    - added in mechanism to search MM chats, pulls by last 45 days
    - trying to fix pipeline issue where `hdbscan` is not installing:
    ```
    Error compiling Cython file:
          ------------------------------------------------------------
          ...
          import numpy as np
          cimport numpy as np
          
          from libc.float cimport DBL_MAX
          
          from dist_metrics cimport DistanceMetric
          ^
          ------------------------------------------------------------
    ```
    
    Tech debt:
    - make a general date range input on the endpoint
    - save embeddings when calculated and pull
    - have a permanent vector store

--- app/aimodels/bertopic/crud/crud_document.py
+++ app/aimodels/bertopic/crud/crud_document.py
@@ -1,10 +1,27 @@
-
 from app.crud.base import CRUDBase
 from ..models.document import DocumentModel
 from ..schemas.document import DocumentCreate
+from sqlalchemy.orm import Session
+from typing import List
+import datetime
 
-# CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType])
+
 class CRUDDocument(CRUDBase[DocumentModel, DocumentCreate, DocumentCreate]):
-    pass
+    def get_by_created_date_range(
+        self, db: Session, *, start_date: datetime.datetime, end_date: datetime.datetime
+    ) -> List[DocumentModel]:
+
+        if not end_date:
+            end_date = datetime.datetime.now()
+
+        if not start_date:
+            start_date = end_date - datetime.timedelta(days=45)
+
+        return (
+            db.query(self.model)
+            .filter(self.model.original_created_time.between(start_date, end_date))
+            .all()
+        )
+
 
 document = CRUDDocument(DocumentModel)
