d3106ec6d79ad8de9a5930b15917941f1e25032b
Emilie Cowen <em23761@mit.edu>
2023-09-23 12:32:37 -0400

    some mattermost mocks for staging tests

--- tests/mattermost/test_mattermost_utils.py
+++ tests/mattermost/test_mattermost_utils.py
@@ -1,9 +1,11 @@
+import uuid
+import pandas as pd
+from unittest.mock import patch, Mock, create_autospec
 from app.core.config import settings, environment_settings
 from app.mattermost.services import mattermost_utils
 
-
-# test mattermost api calls for user, team, and channel info
 def test_mattermost_bot():
+# test mattermost api calls for user, team, and channel info
 
     if environment_settings.environment == 'test':
         return
@@ -31,3 +33,31 @@ def test_mattermost_bot():
         settings.mm_base_url, settings.mm_token, channels['id'].iloc[0])
 
     assert not documents.empty
+
+
+def test_get_user_info(mocker):
+# pipeline safe test for get_user_info
+
+    user = str(uuid.uuid4())
+    mock_data = (dict({'id': user, 'username': user}), pd.DataFrame())
+
+    mocker.patch('app.mattermost.services.mattermost_utils.get_user_info', return_value=mock_data)
+
+    user_info = mattermost_utils.get_user_info("127.0.0.1", "a_token", user)
+
+    assert user_info == mock_data
+
+
+def test_get_all_user_channels(mocker):
+# pipeline safe test for get_all_user_channels
+
+    user = str(uuid.uuid4())
+    team = str(uuid.uuid4())
+    mock_data = pd.DataFrame()
+
+    mocker.patch('app.mattermost.services.mattermost_utils.get_team_channels', return_value=mock_data)
+    mocker.patch('app.mattermost.services.mattermost_utils.get_all_user_channels', return_value=mock_data)
+
+    channel_info = mattermost_utils.get_all_user_channels("127.0.0.1", "a_token", user, [team])
+
+    assert channel_info.equals(mock_data)
