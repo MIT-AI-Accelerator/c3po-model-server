d79992c329d1b0d7aa4a80207097b8c026f5db09
emiliecowen <123681156+emiliecowen@users.noreply.github.com>
2024-01-29 14:20:38 -0500

    Chore/ppg-common-lib (#97)
    - create ppg-common library, include mattermost_utils
    - created new env, confirmed app startup, and pytests passing locally
    
    ---------
    
    Co-authored-by: Gwon, Daniel (DA32459) <Daniel.Gwon@ll.mit.edu>

---
+++ ppg-common/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='ppg-common',
+      version='0.1.0',
+      description='A library for PPG common code',
+      url='--',
+      author='MIT Lincoln Laboratory',
+      license='MIT',
+      packages=['ppg'],
+      include_package_data=True,
+      install_requires = [],
+      zip_safe=True)
