2a7c3a0901c1f2dd0745b8ec8e9ecd2c7a20852e
Daniel Gwon <daniel.gwon@ll.mit.edu>
2024-04-09 11:58:20 -0400

    Use mocker so document upload can be tested on p1

--- tests/mattermost/test_mattermost_router.py
+++ tests/mattermost/test_mattermost_router.py
@@ -1,12 +1,39 @@
-import uuid
+import uuid, datetime
+import pytest
+import pandas as pd
 from sqlalchemy.orm import Session
 from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
 from ppg.core.config import OriginationEnum
 from app.core.config import environment_settings
 from app.mattermost.crud import crud_mattermost
-from app.aimodels.bertopic.crud import crud_document
-from app.aimodels.bertopic.models.document import DocumentModel
-from app.mattermost.models.mattermost_documents import MattermostDocumentModel
+from app.mattermost.models.mattermost_channels import MattermostChannelModel
+from app.mattermost.models.mattermost_users import MattermostUserModel
+
+@pytest.fixture(scope='module')
+def channel_db_obj(db: Session) -> MattermostChannelModel:
+    channel_info = dict(id=str(uuid.uuid4()),
+                        name='my channel',
+                        team_id=str(uuid.uuid4()),
+                        team_name='my team',
+                        display_name='my channel',
+                        type='P',
+                        header='my header',
+                        purpose='my purpose')
+    return crud_mattermost.populate_mm_channel_info(db, channel_info=channel_info)
+
+@pytest.fixture(scope='module')
+def user_db_obj(db: Session) -> MattermostUserModel:
+    user = str(uuid.uuid4())
+    mm_user = dict(id=user,
+                   username=user,
+                   nickname=user,
+                   first_name='Gohan',
+                   last_name='Son',
+                   position='Saiyaman',
+                   email='%s@nitmre.mil' % user)
+    teams = {'0': 'a team', '1': 'b team'}
+    return crud_mattermost.populate_mm_user_info(db, mm_user=mm_user, teams=teams)
 
 # returns 422
 def test_upload_mattermost_user_info_invalid_format(client: TestClient):
@@ -108,24 +135,46 @@ def test_upload_mattermost_documents_invalid_input(client: TestClient):
     assert response.status_code == 422
     assert 'Mattermost' in response.json()['detail']
 
-def test_upload_mattermost_documents_valid_input(db: Session, client: TestClient):
-    channel_info = dict(id=str(uuid.uuid4()),
-                        name='my channel',
-                        team_id=str(uuid.uuid4()),
-                        team_name='my team',
-                        display_name='my channel',
-                        type='P',
-                        header='my header',
-                        purpose='my purpose')
-    channel_db_obj = crud_mattermost.populate_mm_channel_info(db, channel_info=channel_info)
+def test_upload_mattermost_documents_valid_input(channel_db_obj: MattermostChannelModel,
+                                                 user_db_obj: MattermostUserModel,
+                                                 client: TestClient,
+                                                 mocker: MockerFixture):
+    data = {
+        'id': [str(uuid.uuid4())],
+        'create_at': [datetime.datetime.now()],
+        'update_at': [datetime.datetime.now()],
+        'edit_at': [0],
+        'delete_at': [0],
+        'is_pinned': [False],
+        'user_id': [user_db_obj.user_id],
+        'channel_id': [channel_db_obj.channel_id],
+        'channel': [channel_db_obj.id],
+        'root_id': [''],
+        'original_id': [''],
+        'message': ['Kamehameha'],
+        'type': [''],
+        'props': [dict()],
+        'hashtags': [''],
+        'pending_post_id': [''],
+        'reply_count': [0],
+        'last_reply_at': [0],
+        'participants': [''],
+        'metadata': [dict()],
+        'has_reactions': [False],
+        'file_ids': [list()],
+        'datetime': [datetime.datetime.now()]
+    }
+    mock_data = pd.DataFrame(data)
+    mocker.patch('ppg.services.mattermost_utils.get_channel_posts', return_value=mock_data)
 
     response = client.post(
         '/mattermost/documents/upload',
         headers={},
-        json={'channel_ids': [channel_info['id']]}
+        json={'channel_ids': [channel_db_obj.channel_id]}
     )
 
     assert response.status_code == 200
+    assert len(response.json()) > 0
 
 # returns 422
 def test_get_mattermost_documents_invalid_format(client: TestClient):
@@ -148,6 +197,15 @@ def test_get_mattermost_documents_invalid_input(client: TestClient):
 
     assert response.status_code == 422
     assert 'Mattermost' in response.json()['detail']
+
+def test_get_mattermost_documents_no_document(channel_db_obj, client: TestClient):
+    response = client.get('/mattermost/documents/get',
+                          headers={},
+                          params={'team_name': channel_db_obj.team_name,
+                                  'channel_name': channel_db_obj.channel_name})
+
+    assert response.status_code == 422
+    assert 'documents not found' in response.json()['detail']
 
 # returns 422
 def test_mattermost_conversation_thread_invalid_format(client: TestClient):
