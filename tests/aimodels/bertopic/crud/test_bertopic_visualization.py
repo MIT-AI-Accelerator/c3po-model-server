5ed4a55691318ba27ddae9802dc0cf0c79026779
Daniel Gwon <daniel.gwon@ll.mit.edu>
2024-04-18 15:45:15 -0400

    Test validation for model id or name

---
+++ tests/aimodels/bertopic/crud/test_bertopic_visualization.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import Session
+from app.aimodels.bertopic.crud.crud_bertopic_visualization import bertopic_visualization
+from ppg.schemas.bertopic.bertopic_visualization import  BertopicVisualizationTypeEnum
+
+
+def test_get_bertopic_visualization_no_id(db: Session):
+    result = bertopic_visualization.get_by_model_or_topic_id(
+        db, model_or_topic_id='', visualization_type=BertopicVisualizationTypeEnum.MODEL_WORDS)
+    assert result is None
