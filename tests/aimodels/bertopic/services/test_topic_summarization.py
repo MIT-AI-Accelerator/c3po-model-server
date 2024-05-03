e29583f0bc8255f861a2110610a7e0a37b961821
Daniel Gwon <daniel.gwon@ll.mit.edu>
2024-05-03 09:13:57 -0400

    Test llm initialization

--- tests/aimodels/bertopic/services/test_topic_summarization.py
+++ tests/aimodels/bertopic/services/test_topic_summarization.py
@@ -3,19 +3,70 @@ import json
 import pytest
 import pandas as pd
 from datetime import datetime, timedelta
+from pathlib import Path
+from unittest.mock import Mock, MagicMock, create_autospec
+from pytest_mock import MockerFixture
 from fastapi.testclient import TestClient
 from app.main import versioned_app
 from app.aimodels.bertopic.ai_services.topic_summarization import TopicSummarizer, detect_trending_topics, \
-    DEFAULT_PROMPT_TEMPLATE, DEFAULT_REFINE_TEMPLATE
+    DEFAULT_PROMPT_TEMPLATE, DEFAULT_REFINE_TEMPLATE, DEFAULT_LLM_TEMP
+
 
 client = TestClient(versioned_app)
 
+@pytest.fixture(scope='module')
+def mock_model_obj():
+    mock_model_obj = MagicMock()
+    mock_model_obj.model_type = 'mistral'
+    mock_model_obj.id = str(uuid.uuid4())
+    return mock_model_obj
+
+
+@pytest.fixture(scope='module')
+def mock_llm():
+    return MagicMock()
+
 
 # test llm requires initialization
-def test_initalize_llm_invalid():
+def test_initialize_llm_invalid():
     topic_summarizer = TopicSummarizer()
     assert not topic_summarizer.check_parameters(uuid.uuid4(), DEFAULT_PROMPT_TEMPLATE, DEFAULT_REFINE_TEMPLATE)
     assert topic_summarizer.get_summary(['document']) == 'topic summarization disabled'
+
+
+@pytest.mark.parametrize('isfile_side_effect', [
+    (True, False),
+    (False, False),
+    (False, True)
+])
+def test_initialize_llm(mock_s3: MagicMock,
+                        mock_model_obj: MagicMock,
+                        mock_llm: MagicMock,
+                        mocker: MockerFixture,
+                        isfile_side_effect: tuple[bool, bool]):
+
+    # mock external classes and functions we don't want to test
+    mocker.patch('os.path.isfile', side_effect=isfile_side_effect)  # conditional paths determined here
+
+    # don't create any files
+    mocker.patch(
+        'app.aimodels.bertopic.ai_services.topic_summarization.Path', return_value=MagicMock(), autospec=True)
+    mocker.patch('app.core.minio.download_file_from_minio', return_value=None)  # don't touch minio
+    mocker.patch('app.aimodels.bertopic.ai_services.topic_summarization.CTransformers', return_value=mock_llm)
+
+    ts = TopicSummarizer()
+    ts.initialize_llm(mock_s3,
+                      mock_model_obj,
+                      DEFAULT_PROMPT_TEMPLATE,
+                      DEFAULT_REFINE_TEMPLATE,
+                      temp=DEFAULT_LLM_TEMP)
+
+    assert ts.model_type == mock_model_obj.model_type
+    assert ts.model_id == mock_model_obj.id
+    assert ts.llm == mock_llm
+    assert ts.prompt_template == DEFAULT_PROMPT_TEMPLATE
+    assert ts.refine_template == DEFAULT_REFINE_TEMPLATE
+    assert ts.temp == DEFAULT_LLM_TEMP
 
 
 # test acronym expansion
