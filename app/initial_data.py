b1a1b1f57989c4d3f304bbcc99b623e189c5475a
Daniel Gwon <daniel.gwon@ll.mit.edu>
2024-05-14 12:55:21 -0400

    Check for mistral db obj before uploading in staging and prod

--- app/initial_data.py
+++ app/initial_data.py
@@ -286,9 +286,12 @@ def init_llm_pretrained_model(s3: Minio, db: Session) -> None:
     return obj_by_sha
 
 
-def init_gpt4all_db_obj_staging_prod(s3: Minio, db: Session) -> None:
+def init_llm_db_obj_staging_prod(s3: Minio, db: Session, model_enum: LlmFilenameEnum) -> LlmPretrainedModel:
 
-    model_name = "ggml-gpt4all-l13b-snoozy.bin"
+    default_sha256 = settings.default_sha256_l13b_snoozy \
+        if model_enum == LlmFilenameEnum.L13B_SNOOZY \
+        else settings.default_sha256_q4_k_m
+    model_name = model_enum.value
     local_path = os.path.join(
         MODEL_CACHE_BASEDIR, model_name)
 
@@ -303,12 +306,12 @@ def init_gpt4all_db_obj_staging_prod(s3: Minio, db: Session) -> None:
 
     # check to make sure sha256 doesn't already exist
     obj_by_sha: LlmPretrainedModel = gpt4all_crud.llm_pretrained.get_by_sha256(
-        db, sha256=settings.default_sha256_l13b_snoozy)
+        db, sha256=default_sha256)
 
     if not obj_by_sha:
 
         llm_pretrained_obj = LlmPretrainedCreate(
-            sha256=settings.default_sha256_l13b_snoozy, use_base_model=True)
+            sha256=default_sha256, use_base_model=True)
 
         new_llm_pretrained_obj: LlmPretrainedModel = gpt4all_crud.llm_pretrained.create(
             db, obj_in=llm_pretrained_obj)
@@ -449,9 +452,17 @@ def init_large_objects(environment: str, migration_toggle: bool, s3: Minio, db: 
             f"Gpt4All Object ID: {llm_pretrained_obj.id}, SHA256: {llm_pretrained_obj.sha256}")
 
     if (environment == 'staging' or (environment == 'production' and migration_toggle is True)):
+        # Mistral
+        logger.info("Verifying Mistral object in MinIO")
+        llm_pretrained_obj = init_llm_db_obj_staging_prod(s3, db, LlmFilenameEnum.Q4_K_M)
+        logger.info("Verified Mistral object in MinIO.")
+        logger.info(
+            f"Mistral Object ID: {llm_pretrained_obj.id}, SHA256: {llm_pretrained_obj.sha256}")
+
+        # Gpt4All
         logger.info("Verifying Gpt4All object in MinIO")
-        llm_pretrained_obj = init_gpt4all_db_obj_staging_prod(s3, db)
-        logger.info("Verified Gpt4All object in MinIO")
+        llm_pretrained_obj = init_llm_db_obj_staging_prod(s3, db, LlmFilenameEnum.L13B_SNOOZY)
+        logger.info("Verified Gpt4All object in MinIO.")
         logger.info(
             f"Gpt4All Object ID: {llm_pretrained_obj.id}, SHA256: {llm_pretrained_obj.sha256}")
     ########## large object uploads ################
