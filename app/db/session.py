75ff14a44906515adae433d4f4c464fa7c2cdb75
emiliecowen <123681156+emiliecowen@users.noreply.github.com>
2024-06-24 13:06:17 -0400

    feat/db-download (#218)
    - endpoint to extract db contents
    https://github.com/orgs/MIT-AI-Accelerator/projects/3/views/3?pane=issue&itemId=63055414
    - bertopic sync with 577th (umap and seed topics)
    https://github.com/orgs/MIT-AI-Accelerator/projects/3/views/3?pane=issue&itemId=68221318
    - remove update_topics
    https://github.com/orgs/MIT-AI-Accelerator/projects/3/views/3?pane=issue&itemId=68160526
    - add channel_name to topic details
    https://github.com/orgs/MIT-AI-Accelerator/projects/3/views/3?pane=issue&itemId=68148606
    - end-to-end and pytests passing

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
 engine = create_engine(settings.sqlalchemy_database_uri, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+inspector = inspect(engine)
+table_names = inspector.get_table_names(schema='public')
