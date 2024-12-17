0ff7396f14172fd95d9a1460dea183fd714adaee
Emilie Cowen <em23761@mit.edu>
2024-12-17 15:43:53 -0500

    pydantic v2 upgrade almost finished, tensorflow not working on macos

--- app/ppg_common/schemas/bertopic/bertopic_embedding_pretrained.py
+++ app/ppg_common/schemas/bertopic/bertopic_embedding_pretrained.py
@@ -1,7 +1,7 @@
 import re
 import enum
 from typing import Optional
-from pydantic import BaseModel, UUID4, validator
+from pydantic import BaseModel, UUID4, field_validator, ConfigDict
 from app.core.config import OriginationEnum
 
 class EmbeddingModelTypeEnum(str, enum.Enum):
@@ -18,7 +18,7 @@ class BertopicEmbeddingPretrainedBase(BaseModel):
     reference: Optional[dict] = {}
 
     # ensure valid sha256 format
-    @validator('sha256')
+    @field_validator('sha256')
     def sha256_must_be_valid(cls, v):
         # pylint: disable=no-self-argument
 
@@ -53,8 +53,7 @@ class BertopicEmbeddingPretrainedInDBBase(BertopicEmbeddingPretrainedBase):
     sha256: str
     originated_from: OriginationEnum
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
 
 # Properties to return to client
 class BertopicEmbeddingPretrained(BertopicEmbeddingPretrainedInDBBase):
