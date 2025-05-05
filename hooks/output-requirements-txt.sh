4eafc0ca8aeca7cf53190fbf94f72441263f5381
emiliecowen <123681156+emiliecowen@users.noreply.github.com>
2025-05-05 09:22:55 -0400

    chore/so-many-failures2 (#288)
    - second round of module upgrades for p1
    - end-to-end and pytests passing

--- hooks/output-requirements-txt.sh
+++ hooks/output-requirements-txt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 poetry export -o requirements.txt --without-hashes --with test
+
+# not proud of this, but not sure how else to exclude aiohttp (unused and causing a p1 pipeline failure
+sed -i '' '/aiohttp/d' requirements.txt
