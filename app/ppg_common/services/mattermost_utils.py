326c64df7cdbe4865c2a9d24f74385fab19d1353
Emilie Cowen <em23761@mit.edu>
2025-10-28 13:31:37 -0400

    remove retry from mm get_all_pages

--- app/ppg_common/services/mattermost_utils.py
+++ app/ppg_common/services/mattermost_utils.py
@@ -41,22 +41,20 @@ def get_all_pages(url, mm_token, is_channel=False, do_pagination=True):
             if rlen < per_page:
                 break
 
-        except requests.exceptions.ReadTimeout:
-            logger.error(f"{resp.url} request timed out: {resp.status_code}. {e}")
+        except requests.exceptions.ReadTimeout as e:
+            logger.error(f"{url} request timed out: {resp.status_code}. {e}")
             logger.error(f"response headers: {resp.headers}")
             logger.error(f"response content: {resp.text}")
-            logger.debug(f"retrying page {page_num}...")
-            tm.sleep(60)
-            continue
+            break
 
         except requests.exceptions.HTTPError as e:
-            logger.error(f"{resp.url} request failed: {resp.status_code}. {e}")
+            logger.error(f"{url} request failed: {resp.status_code}. {e}")
             logger.error(f"response headers: {resp.headers}")
             logger.error(f"response content: {resp.text}")
             break
 
         except Exception as e:
-            logger.error(f"{resp.url} unexpected error: {e}")
+            logger.error(f"{url} unexpected error: {e}")
             break
 
         page_num += 1
